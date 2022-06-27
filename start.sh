c6bfef897eb53e9ccfa1fce33580a344d585947a
Akshay-Chand <107416405+Akshay-Chand@users.noreply.github.com>
2022-06-27 15:08:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/tezzarobot/pooja-hegde.git /PROFESSOR-BOT
+  git clone https://github.com/Akshay-Chand/pooja-hegde.git /PROFESSOR-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /PROFESSOR-BOT
 fi
 cd /PROFESSOR-BOT
 pip3 install -U -r requirements.txt
-echo "Starting 𝙿𝚘𝚘𝚓𝚊 𝙷𝚎𝚐𝚍𝚎....🔥🔥"
+echo "Starting Rashmika....💘💘"
 python3 bot.py
