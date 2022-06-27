e6067aa24008a2961107cb3e55efcb007915e6c0
Akshay-Chand <107416405+Akshay-Chand@users.noreply.github.com>
2022-06-27 15:46:03 +0530

    Update song.py

--- plugins/admemes/song.py
+++ plugins/admemes/song.py
@@ -49,7 +49,7 @@ def song(client, message):
         open(thumb_name, 'wb').write(thumb.content)
 
 
-        performer = f"[ᗩᒍᗩ᙭]" 
+        performer = f"[𝗥𝗔𝗦𝗛𝗠𝗜𝗞𝗔]" 
         duration = results[0]["duration"]
         url_suffix = results[0]["url_suffix"]
         views = results[0]["views"]
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**BY›› [Mᴋɴ Bᴏᴛᴢ™](https://t.me/mkn_bots_updates)**'
+        rep = '**BY›› [𝚁𝙼 𝚂𝚃𝚄𝙳𝙸𝙾𝚂](https://t.me/RM_STUDIOSZ)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
