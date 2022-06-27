a3b12ae80b8034dd5bb969f5dd2e0d7b95d8ea24
Akshay-Chand <107416405+Akshay-Chand@users.noreply.github.com>
2022-06-27 15:39:22 +0530

    Update Lyrics.py

--- plugins/Lyrics.py
+++ plugins/Lyrics.py
@@ -35,5 +35,5 @@ def lyrics(song):
         fin = search(song)
         text = f'**🎶 Successfully Extracted Lyrics Of {song} 🎶**\n\n'
         text += f'`{fin["lyrics"]}`'
-        text += '\n\n\n**Made By 𝙋𝙤𝙤𝙟𝙖 𝙝𝙚𝙜𝙙𝙚**'
+        text += '\n\n\n**Made By 𝗥𝗔𝗦𝗛𝗠𝗜𝗞𝗔**'
         return text
