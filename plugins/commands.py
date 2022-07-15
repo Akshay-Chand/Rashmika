c8192ceed93d178daea2769c0ae448692198166c
Akshay-Chand <107416405+Akshay-Chand@users.noreply.github.com>
2022-07-15 18:45:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -248,6 +248,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton(' 💌 Update 💌', url='https://t.me/RM_STUDIOSZ') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
